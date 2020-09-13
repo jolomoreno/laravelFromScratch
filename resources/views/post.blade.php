850a576c6e7b3d3a944911c7c09d77afa12d984e
jolomoreno <jolorenzom@gmail.com>
2020-09-13 13:26:42 +0200

    First commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta
+            name="viewport"
+            content="width= device-with, user-scalable=no, inital-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+        >
+        <meta
+            http-equiv="X-UA-Compatible"
+            content="ie=edge"
+        >
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>My Blog Post</h1>
+        <p>{{ $postVariable }}</p>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('test', function () {
+    $name = request('name');
+    return view('test', [
+        'name'=> $name
+    ]);
+});
+
+Route::get('/posts/{post}', function ($post) {
+    $posts = [
+        'my-first-post' => 'Hello, this is my first post',
+        'my-second-post' => 'Hello, this is my second post'
+    ];
+    if (! array_key_exists($post, $posts)) {
+        abort(404, 'Sorry, that post was not found');
+    }
+    return view('post', [
+       'postVariable' => $posts[$post] ]);
+});
