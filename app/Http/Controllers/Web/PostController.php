07ed16a7744849839ce07941a8218b88464ef4ed
jolomoreno <jolorenzom@gmail.com>
2020-09-13 14:35:37 +0200

    Get data from DB

--- app/Http/Controllers/Web/PostController.php
+++ app/Http/Controllers/Web/PostController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function show($post)
     {
-        $posts = [
-            'my-first-post' => 'Hello, this is my first post',
-            'my-second-post' => 'Hello, this is my second post'
-        ];
+//        $posts = [
+//            'my-first-post' => 'Hello, this is my first post',
+//            'my-second-post' => 'Hello, this is my second post'
+//        ];
+        $post = DB::table('posts')->where('slug', $post)->first();
+
+        dd($post);
+
         if (! array_key_exists($post, $posts)) {
             abort(404, 'Sorry, that post was not found');
         }
